3c7d3a8ba07c8acf274642308aae53a7f047b1bc
programpkpt <programpkpt@gmail.com>
2024-11-17 21:07:05 +0700

    update dark mode + perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
-      padding: '50px',
+      padding: '24px',
     },
     extend: {
       colors: {
